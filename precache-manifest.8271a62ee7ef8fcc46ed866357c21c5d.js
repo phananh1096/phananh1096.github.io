d8650a315c6c3dc1ae78d2869db765aba71921c5
Phan Anh Nguyen <phan.nguyen@tufts.edu>
2020-09-04 23:05:15 -0400

    Updates

---
+++ precache-manifest.8271a62ee7ef8fcc46ed866357c21c5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320137bacd117ef3f4aad5bb1dd8fecf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa460b58a714c02fb6b",
+    "url": "/static/css/2.1f2b2ec0.chunk.css"
+  },
+  {
+    "revision": "0dd8bcb8a9880f39cb42",
+    "url": "/static/css/main.52cf25a3.chunk.css"
+  },
+  {
+    "revision": "4aa460b58a714c02fb6b",
+    "url": "/static/js/2.2b2a1626.chunk.js"
+  },
+  {
+    "revision": "633ce508062cdafc4f8ece51f4d45ac9",
+    "url": "/static/js/2.2b2a1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd8bcb8a9880f39cb42",
+    "url": "/static/js/main.5fe7bb1a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9d9002961ae2a6f83d70a9964066220f",
+    "url": "/static/media/profile.9d900296.jpg"
+  },
+  {
+    "revision": "a238efa2b9dc6286c2516a4acfeb15a0",
+    "url": "/static/media/whitesmoke.a238efa2.jpg"
+  }
+]);
